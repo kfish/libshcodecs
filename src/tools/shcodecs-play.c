4898b6c2dbcd74ba8436fb1d91191c750fa5cded
Phil Edworthy <phil.edworthy@renesas.com>
2009-06-17 00:27:19 +0100

    Added shcodecs-play tool
    The shcodecs-play tool is essentially the same as shcodecs-dec, though it uses aio to read the stream in parallel to decode and runs the veu in a separate thread to output to the framebuffer. It also includes code to use framebuffer panning, if available.

---
+++ src/tools/shcodecs-play.c
@@ -0,0 +1,612 @@
+/*
+ * libshcodecs: A library for controlling SH-Mobile hardware codecs
+ * Copyright (C) 2009 Renesas Technology Corp.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA  02110-1301 USA
+ */
+
+/*
+ * Program to decode video and show on the display.
+ *
+ * This plays an Elementary Stream. Whilst it is possible to get the width & 
+ * height of the video from this, libshcodecs does not support this as the
+ * information is available in the file container. Therefore, for streams
+ * other than QVGA, you must specify the width & height of the video being
+ * decoded.
+ *
+ * The code relies on the fact that the VPU never writes the same output buffer
+ * for two consecutive frames that are output. This allows the output buffer to
+ * be treated as though it is double-buffered, i.e. whilst the VPU is decoding
+ * the next frame, in parallel the image is sent to the display.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stropts.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <sys/param.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <linux/fb.h>
+#include <linux/ioctl.h>
+#include <aio.h>
+#include <pthread.h>
+#include <errno.h>
+#include <shcodecs/shcodecs_decoder.h>
+#include "veu_colorspace.h"
+
+#define DEFAULT_WIDTH 320
+#define DEFAULT_HEIGHT 240
+#define DEFAULT_FPS	25
+
+/* The size of NALs/VOPs varies, it depends on the profile & level */
+/* For worst case NAl size see MaxCPB in the H.264 spec */
+#define MAX_NAL_SIZE (256 * 1024)
+
+/* #define _DEBUG */
+
+/* limitation of VEU2H on SH7723 */
+#define VEU2H_MAX_WIDTH 640
+
+#define LCD_BPP 2
+#define U_SEC_PER_SEC 1000000
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+struct private_data {
+	/* Display data */
+	int fb_handle;
+	int fb_index;
+	struct fb_fix_screeninfo fb_fix;
+	struct fb_var_screeninfo fb_var;
+	unsigned char *fb_base;
+	unsigned char *fb_screenMem;
+
+	/* Output thread related data */
+	pthread_mutex_t output_mutex;
+	pthread_mutex_t output_done_mutex;
+	unsigned char *p_frame_y;
+	unsigned char *p_frame_c;
+
+	/* File read & input data */
+	struct aiocb read_aiocb;
+	unsigned char *input_buf;
+	unsigned char *read_buf;
+	int input_buf_len;
+
+	struct timeval tlast;        /* Timestamp of last frame output */
+	struct timeval tframedelta;  /* Time between frames (1/fps) */
+
+	int lcd_w;          /* LCD size */
+	int lcd_h;
+
+	int dst_w;          /* Size of video output */
+	int dst_h;
+
+	int src_w;          /* Size of video input */
+	int src_h;
+
+	int fps;            /* Playback speed; frames per second */
+	int frames_output;  /* Number of times the frame decoded callback is called */
+	int frames_dropped; /* Number of times the frame arrives late */
+};
+
+
+
+void debug_printf(__const char *__restrict __format, ...)
+{
+#ifdef _DEBUG
+	printf(__format);
+#endif
+}
+
+static struct option stLong_options[] = {
+	{ "format", 1, 0, 'f'},
+	{ "input" , 1, 0, 'i'},
+	{ "width" , 1, 0, 'w'},
+	{ "height", 1, 0, 'h'},
+	{ "size"  , 1, 0, 's'},
+};
+
+static void
+usage (const char *progname)
+{
+  printf ("Usage: %s [options] ...\n", progname);
+  printf ("Decode a MPEG-4 or H.264 elementry stream and show on the LCD.\n");
+  printf ("\nFile options\n");
+  printf ("  -r               Set the playback speed, frames per second\n");
+  printf ("  -i, --input      Set the video input filename\n");
+  printf ("\nEncoding format\n");
+  printf ("  -f, --format     The file format [h264, mpeg4]\n");
+  printf ("\nDimensions of encoded stream\n");
+  printf ("  -w, --width      The image width in pixels of the file\n");
+  printf ("  -h, --height     The image height in pixels of the file\n");
+  printf ("  -s, --size       Set the input image size [qcif, cif, qvga, vga]\n");
+  printf ("\nDimensions of video on the display\n");
+  printf ("  -x,              The image width in pixels on the display\n");
+  printf ("  -y,              The image height in pixels on the display\n");
+}
+
+/*****************************************************************************/
+
+static int display_flip(struct private_data *pvt)
+{
+	struct fb_var_screeninfo fb_screen = pvt->fb_var;
+
+	fsync(pvt->fb_handle);
+
+	fb_screen.xoffset = 0;
+	fb_screen.yoffset = 0;
+	if (pvt->fb_index==0)
+		fb_screen.yoffset = pvt->fb_var.yres;
+	if (-1 == ioctl(pvt->fb_handle, FBIOPAN_DISPLAY, &fb_screen))
+		return 0;
+
+	/* Point to the back buffer */
+	pvt->fb_screenMem = pvt->fb_base;
+	if (pvt->fb_index!=0)
+		pvt->fb_screenMem += pvt->fb_var.yres * pvt->fb_var.xres * LCD_BPP;
+
+	pvt->fb_index = (pvt->fb_index+1) & 1;
+	return 1;
+}
+
+
+static int display_open(struct private_data *pvt, char *device)
+{
+	if ((pvt->fb_handle = open(device, O_RDWR)) < 0) {
+		fprintf(stderr, "Open %s: %s.\n", device, strerror(errno));
+		return 0;
+	}
+	if (ioctl(pvt->fb_handle, FBIOGET_FSCREENINFO, &pvt->fb_fix) < 0) {
+		fprintf(stderr, "Ioctl FBIOGET_FSCREENINFO error.\n");
+		return 0;
+	}
+	if (ioctl(pvt->fb_handle, FBIOGET_VSCREENINFO, &pvt->fb_var) < 0) {
+		fprintf(stderr, "Ioctl FBIOGET_VSCREENINFO error.\n");
+		return 0;
+	}
+	if (pvt->fb_fix.type != FB_TYPE_PACKED_PIXELS) {
+		fprintf(stderr, "This program can only handle packed pixel frame buffers.\n");
+		return 0;
+	}
+
+	pvt->lcd_w = pvt->fb_var.xres;
+	pvt->lcd_h = pvt->fb_var.yres;
+
+	pvt->fb_screenMem = pvt->fb_base = (unsigned char*)pvt->fb_fix.smem_start;
+	pvt->fb_index = 0;
+	display_flip(pvt);
+
+	return 1;
+}
+
+static void display_close(struct private_data *pvt)
+{
+	close(pvt->fb_handle);
+}
+
+/*****************************************************************************/
+
+/*
+ * Get the next pointer to the input bitstream after decoding a frame/slice.
+ * consumed: the number of bytes that have just been consumed.
+ *
+ * The decoder must be presented with compressed data that consists of at least
+ * one whole unit (i.e. a NAL or VOP).
+ */
+static int update_input(struct private_data *pvt, int consumed)
+{
+	struct aiocb *aio = &pvt->read_aiocb;
+	int read_len, ret;
+
+	/* Copy the unused stream data to the start of the buffer */
+	pvt->input_buf_len -= consumed;
+	memmove(pvt->input_buf, pvt->input_buf+consumed, pvt->input_buf_len);
+
+	if (pvt->input_buf_len < MAX_NAL_SIZE)
+	{
+		/* Wait for pending read to complete */
+		aio_suspend(&aio, 1, NULL);
+
+		read_len = aio_return(aio);
+		if (read_len > 0)
+		{
+			/* Copy the new stream data after the unused data */
+			memcpy(pvt->input_buf + pvt->input_buf_len, pvt->read_buf, read_len);
+			pvt->input_buf_len += read_len;
+
+			aio->aio_nbytes = MAX_NAL_SIZE;
+			aio->aio_offset += read_len;
+
+			/* Start a new read */
+			ret = aio_read(aio);
+			if (ret < 0) {
+				fprintf(stderr, "aio_read failed!\n");
+				return ret;
+			}
+		}
+	}
+
+	return pvt->input_buf_len;
+}
+
+static int file_read_init(struct private_data *pvt, char *video_filename)
+{
+	struct aiocb *aio = &pvt->read_aiocb;
+	int ret;
+
+	pvt->input_buf_len = 0;
+	bzero((char*)aio, sizeof(struct aiocb));
+
+	aio->aio_fildes = open(video_filename, O_RDONLY);
+	if (aio->aio_fildes == -1) {
+		fprintf(stderr, "Open %s: %s.\n", video_filename, strerror(errno));
+		return -1;
+	}
+
+	/* The input buffer is twice the size of the largest NAL/VOP becuase we
+	   could have 99% of one NAL/VOP unused in the buffer and need to store
+	   the next read data after this */
+	pvt->input_buf = malloc(MAX_NAL_SIZE * 2);
+	pvt->read_buf  = malloc(MAX_NAL_SIZE);
+	if (pvt->input_buf == NULL || pvt->read_buf == NULL ) {
+		fprintf(stderr, "Memory allocation failed!\n");
+		return -1;
+	}
+
+	aio->aio_buf = pvt->read_buf;
+	aio->aio_nbytes = MAX_NAL_SIZE;
+	aio->aio_offset = 0;
+
+	/* Start a new read */
+	ret = aio_read(&pvt->read_aiocb);
+	if (ret < 0) {
+		fprintf(stderr, "aio_read failed!\n");
+		return ret;
+	}
+
+	/* Wait for initial read to complete */
+	return update_input(pvt, 0);
+}
+
+static void file_read_deinit(struct private_data *pvt)
+{
+	close(pvt->read_aiocb.aio_fildes);
+	free(pvt->input_buf);
+	free(pvt->read_buf);
+}
+
+
+/*****************************************************************************/
+
+static int time_diff(struct timeval *start, struct timeval *end, struct timeval *diff)
+{
+	if ((end->tv_usec - start->tv_usec) < 0) {
+		diff->tv_sec  = end->tv_sec - start->tv_sec - 1;
+		diff->tv_usec = U_SEC_PER_SEC + end->tv_usec - start->tv_usec;
+	} else {
+		diff->tv_sec  = end->tv_sec  - start->tv_sec;
+		diff->tv_usec = end->tv_usec - start->tv_usec;
+	}
+
+	if (diff->tv_sec < 0)
+		return -1;
+	if ((diff->tv_sec == 0) && (diff->tv_usec < 0))
+		return -1;
+	return 1;
+}
+
+static void time_add(struct timeval *start, struct timeval *offset, struct timeval *newtime)
+{
+	newtime->tv_sec  = start->tv_sec  + offset->tv_sec;
+	newtime->tv_usec = start->tv_usec + offset->tv_usec;
+
+	if (newtime->tv_usec > U_SEC_PER_SEC) {
+		newtime->tv_sec++;
+		newtime->tv_usec -= U_SEC_PER_SEC;
+	}
+}
+
+
+/*****************************************************************************/
+
+void *output_thread(void *data)
+{
+	unsigned char *y_buf;
+	unsigned char *c_buf;
+	int dst_offset;
+	struct private_data *pvt = (struct private_data*)data;
+
+	/* Offset for centred output */
+	int x_offset = ((pvt->lcd_w - pvt->dst_w) / 2) & ~16;
+	int y_offset = ((pvt->lcd_h - pvt->dst_h) / 2) & ~16;
+
+	while(1) {
+		/* Wait for new frame from the decoder */
+		pthread_mutex_lock(&pvt->output_mutex);
+
+		/* Copy shared variables */
+		y_buf = pvt->p_frame_y;
+		c_buf = pvt->p_frame_c;
+
+		/* Use the VEU to scale & perform colour space conversion */
+		dst_offset = ((y_offset * pvt->lcd_w) + x_offset) * LCD_BPP;
+		sh_veu_operation(0, y_buf, c_buf,
+			      pvt->src_w, pvt->src_h, pvt->src_w, YCbCr420,
+			      pvt->fb_screenMem + dst_offset, NULL,
+			      pvt->dst_w, pvt->dst_h, pvt->lcd_w, RGB565, 0);
+
+		display_flip(pvt);
+		pthread_mutex_unlock(&pvt->output_done_mutex);
+	}
+
+	pthread_exit(NULL);
+}
+
+static void
+frame_ready(struct private_data *pvt, unsigned char *y_in, unsigned char *c_in)
+{
+	/* Ensure the output thread is ready, we don't want the decoder running
+        faster than the output */
+	pthread_mutex_lock(&pvt->output_done_mutex);
+
+	/* Variables shared between the threads */
+	pvt->p_frame_y = y_in;
+	pvt->p_frame_c = c_in;
+
+	/* Signal the output thread that a frame is available */
+	pthread_mutex_unlock(&pvt->output_mutex);
+}
+
+
+/* local output callback */
+static int
+local_vpu4_decoded (SHCodecs_Decoder *decoder,
+	unsigned char *y_buf, int y_size,
+	unsigned char *c_buf, int c_size,
+	void *user_data)
+{
+	struct timeval tcurrent, tsleep, texpected;
+	struct private_data *pvt = (struct private_data*)user_data;
+
+	pvt->frames_output++;
+
+	time_add(&pvt->tlast, &pvt->tframedelta, &texpected);
+	gettimeofday(&tcurrent, 0);
+	if (time_diff(&tcurrent, &texpected, &tsleep) < 0)
+		pvt->frames_dropped++;
+	else
+		usleep ((useconds_t)tsleep.tv_usec);
+
+	frame_ready(pvt, y_buf, c_buf);
+	gettimeofday(&pvt->tlast, 0);
+
+	return 0;
+}
+
+
+/*****************************************************************************/
+
+int main(int argc, char **argv)
+{
+	SHCodecs_Decoder *decoder;
+	int stream_type = SHCodecs_Format_H264;
+	int i, rc, bytes_decoded;
+	char c, video_filename[MAXPATHLEN];
+	struct timeval start, end, duration;
+	struct private_data pvt_data;
+	struct private_data *pvt;
+	pthread_t thread_output;
+
+	if (argc == 1) {
+		usage(argv[0]);
+		exit(0);
+	}
+
+	pvt = &pvt_data;
+
+	/* Initialize display */
+	if (!display_open(pvt, "/dev/fb")) {
+		fprintf(stderr, "Unable to initialise display\n");
+		exit(-3);
+	}
+
+	setvbuf(stdout, NULL, _IONBF, 0);
+	video_filename[MAXPATHLEN-1] = '\0';
+
+	/* Set defaults */
+	pvt->src_w  = DEFAULT_WIDTH;
+	pvt->src_h = DEFAULT_HEIGHT;
+	pvt->dst_w = pvt->lcd_w;
+	pvt->dst_h = pvt->lcd_h;
+	pvt->fps = DEFAULT_FPS;
+
+	while (1) {
+		c = getopt_long(argc, argv, "f:i:w:h:r:x:y:a:s", stLong_options, &i);
+		if (c == -1)
+			break;
+
+		switch (c) {
+		case 'f':
+			if (strncmp(optarg, "mpeg4", 5) == 0)
+				stream_type = SHCodecs_Format_MPEG4;
+			else if (strncmp(optarg, "h264", 4) == 0)
+				stream_type = SHCodecs_Format_H264;
+			else{
+				debug_printf("argument: Unknown video format: %s.\n",optarg);
+				exit(-1);
+			}
+			break;
+		case 'i':
+			if (optarg)
+				strncpy(video_filename, optarg, MAXPATHLEN-1);
+			break;
+		case 'w':
+			if (optarg)
+				pvt->src_w = strtoul(optarg, NULL, 10);
+			break;
+		case 'h':
+			if (optarg)
+				pvt->src_h = strtoul(optarg, NULL, 10);
+			break;
+		case 'r':
+			if (optarg)
+				pvt->fps = strtoul(optarg, NULL, 10);
+			break;
+		case 'x':
+			if (optarg)
+				pvt->dst_w = strtoul(optarg, NULL, 10);
+				if (pvt->dst_w > pvt->lcd_w) {
+					pvt->dst_w = pvt->lcd_w;
+					debug_printf("Limiting output width to LCD width (%d)\n", pvt->dst_w);
+				}
+			break;
+		case 'y':
+			if (optarg)
+				pvt->dst_h = strtoul(optarg, NULL, 10);
+				if (pvt->dst_h > pvt->lcd_h) {
+					pvt->dst_h = pvt->lcd_h;
+					debug_printf("Limiting output height to LCD height (%d)\n", pvt->dst_h);
+				}
+			break;
+		case 's':
+			if (optarg) {
+				if (!strncasecmp (optarg, "qcif", 4)) {
+					pvt->dst_w = 176;
+					pvt->dst_h = 144;
+				} else if (!strncmp (optarg, "cif", 3)) {
+					pvt->dst_w = 352;
+					pvt->dst_h = 288;
+				} else if (!strncmp (optarg, "qvga", 4)) {
+					pvt->dst_w = 320;
+					pvt->dst_h = 240;
+				} else if (!strncmp (optarg, "vga", 3)) {
+					pvt->dst_w = 640;
+					pvt->dst_h = 480;
+				}
+			}
+			break;
+		default:
+			usage(argv[0]);
+			exit(-2);
+		}
+	}
+	if (pvt->src_w == -1 || pvt->src_h == -1){
+		fprintf(stderr, "Invalid source width and/or height specified.\n");
+		exit(-3);
+	}
+	if ( (strcmp(video_filename, "-") == 0) || (video_filename[0] == '\0') ){
+		fprintf(stderr, "Invalid input file.\n");
+		exit(-4);
+	}
+	if (pvt->src_w < SHCODECS_MIN_FX || pvt->src_w > SHCODECS_MAX_FX
+	    || pvt->src_h < SHCODECS_MIN_FY || pvt->src_h > SHCODECS_MAX_FY) {
+		fprintf(stderr, "Invalid source width and/or height specified.\n");
+		exit(-6);
+	}
+	if (optind > argc){
+		fprintf(stderr, "Too many arguments.\n");
+		exit(-7);
+	}
+
+	/* Ensure the output is no bigger than the LCD */
+	pvt->dst_w = min(pvt->dst_w, pvt->lcd_w);
+	pvt->dst_h = min(pvt->dst_h, pvt->lcd_h);
+
+	/* If scaling down and destination is larger than VGA width, limit to VGA */
+	if (pvt->src_w > pvt->dst_w)
+		pvt->dst_w = min(pvt->dst_w, VEU2H_MAX_WIDTH);
+	if (pvt->src_h > pvt->dst_h)
+		pvt->dst_h = min(pvt->dst_h, VEU2H_MAX_WIDTH);
+
+	debug_printf("Format: %s\n", stream_type == SHCodecs_Format_H264 ? "H.264" : "MPEG4");
+	debug_printf("File resolution:    %dx%d\n", pvt->src_w, pvt->src_h);
+	debug_printf("Display resolution: %dx%d\n", pvt->lcd_w, pvt->lcd_h);
+	debug_printf("Output resolution:  %dx%d\n", pvt->dst_w, pvt->dst_h);
+	debug_printf("Input video file: %s\n", video_filename);
+
+
+	/* Output thread initialisation */
+	pthread_mutex_init(&pvt->output_mutex, NULL);
+	pthread_mutex_lock(&pvt->output_mutex);
+	pthread_mutex_init(&pvt->output_done_mutex, NULL);
+
+	rc = pthread_create(&thread_output, NULL, output_thread, pvt);
+	if (rc){
+		fprintf(stderr, "Creating output thread failed, exiting\n");
+		exit(-1);
+	}
+
+	/* Open the VEU UIO */
+	sh_veu_open ();
+
+	/* Open file descriptors to talk to the VPU driver */
+	if ((decoder = shcodecs_decoder_init(pvt->src_w, pvt->src_h, stream_type)) == NULL)
+		exit (-9);
+
+	/* setup callback for frame decoded  & make the decode use physical addresses */
+	pvt->frames_output = 0;
+	pvt->frames_dropped = 0;
+	shcodecs_decoder_set_decoded_callback (decoder, local_vpu4_decoded, pvt);
+	shcodecs_decoder_set_use_physical (decoder, 1);
+
+	/* File read initialisation */
+	if (file_read_init(pvt, video_filename) < 0)
+		exit(-1);
+
+	/* Time for 1 frame = 1/fps */
+	pvt->tframedelta.tv_sec  = 0;
+	pvt->tframedelta.tv_usec = U_SEC_PER_SEC / pvt->fps;
+
+	gettimeofday(&start, 0);
+	gettimeofday(&pvt->tlast, 0);
+
+	/* decode main loop */
+	do {
+		bytes_decoded = shcodecs_decode(decoder, pvt->input_buf, pvt->input_buf_len);
+
+	} while (bytes_decoded > 0 && update_input(pvt, bytes_decoded) > 0);
+
+	/* Finalize the decode output, in case a final MPEG4 frame is available */
+	shcodecs_decoder_finalize (decoder);
+
+	gettimeofday(&end, 0);
+
+	/* Wait for output thread to finish */
+	pthread_mutex_lock(&pvt->output_done_mutex);
+
+	shcodecs_decoder_close(decoder);
+	file_read_deinit(pvt);
+	sh_veu_close();
+	display_close(pvt);
+
+	pthread_mutex_destroy(&pvt->output_mutex);
+	pthread_mutex_destroy(&pvt->output_done_mutex);
+
+	time_diff(&start, &end, &duration);
+	debug_printf("Frames output = %d\n", pvt->frames_output);
+	debug_printf("Playback time = %d seconds\n", duration.tv_sec); 
+	debug_printf("FPS = %10f\n", (double)pvt->frames_output/duration.tv_sec);
+	debug_printf("Late frames = %d\n", pvt->frames_dropped);
+
+	return 0;
+}
+
