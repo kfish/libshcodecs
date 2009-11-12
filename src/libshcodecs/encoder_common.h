c1023c0c21a9e94c69711a7d99d88ba793f81be8
Phil Edworthy <phil.edworthy@renesas.com>
2009-11-12 15:34:53 +0000

    Removed defines that aren't used.

--- src/libshcodecs/encoder_common.h
+++ src/libshcodecs/encoder_common.h
@@ -31,11 +31,6 @@
 #ifdef CAPTURE_720P
 
 /*----------------------------------------------------------*/
-/*       Image width x Image height x 1.5                   */
-/*----------------------------------------------------------*/
-#define WIDTH_HEIGHT_1_5	1382400	/* 622080=1280 x 720 x 1.5(D1 Size) */
-#define MAX_FRAME 2
-/*----------------------------------------------------------*/
 /*       Other work-field area size                         */
 /*----------------------------------------------------------*/
 #define MY_WORK_AREA_SIZE	1024*512
@@ -46,11 +41,7 @@
 #define MY_STREAM_BUFF_SIZE	1024*256     /* byte unit */	/* this value must be multiple of 32 */
 
 #elif defined(MAX_D1)
-/*----------------------------------------------------------*/
-/*       Image width x Image height x 1.5                   */
-/*----------------------------------------------------------*/
-#define WIDTH_HEIGHT_1_5	622080	/* 622080=720 x 576 x 1.5(D1 Size) */
-#define MAX_FRAME 2
+
 /*----------------------------------------------------------*/
 /*       Other work-field area size                         */
 /*----------------------------------------------------------*/
@@ -63,11 +54,6 @@
 
 #elif defined(CAPTURE_VGA)
 /*----------------------------------------------------------*/
-/*       Image width x Image height x 1.5                   */
-/*----------------------------------------------------------*/
-#define WIDTH_HEIGHT_1_5	460800	/* 460800=640 x 480 x 1.5(VGA Size) */
-#define MAX_FRAME 2
-/*----------------------------------------------------------*/
 /*       Other work-field area size                         */
 /*----------------------------------------------------------*/
 #define MY_WORK_AREA_SIZE	(101376*4) /*76800 One stream */	/* QVGA */
@@ -78,11 +64,6 @@
 #define MY_STREAM_BUFF_SIZE	(400000)     /* byte unit */	/* this value must be multiple of 32 */
 
 #else				/* Not D1, or CAPTURE_VGA */
-/*----------------------------------------------------------*/
-/*       Image width x Image height x 1.5                   */
-/*----------------------------------------------------------*/
-#define WIDTH_HEIGHT_1_5	152064	/* 115200=320 x 240 x 1.5(QVGA Size) */
-#define MAX_FRAME 4
 /*----------------------------------------------------------*/
 /*       Other work-field area size                         */
 /*----------------------------------------------------------*/
@@ -112,9 +93,9 @@
 /*----------------------------------------------------------*/
 /*       SEI-stream-output buffer size                      */
 /*----------------------------------------------------------*/
-							 /*#define MY_SEI_STREAM_BUFF_SIZE       175000 *//* Level 1 */
-							  /*#define MY_SEI_STREAM_BUFF_SIZE       2000000 *//* Level 2 */
-							   /*#define MY_SEI_STREAM_BUFF_SIZE       10000000 *//* Level 3 */
+/*#define MY_SEI_STREAM_BUFF_SIZE       175000 *//* Level 1 */
+/*#define MY_SEI_STREAM_BUFF_SIZE       2000000 *//* Level 2 */
+/*#define MY_SEI_STREAM_BUFF_SIZE       10000000 *//* Level 3 */
 #define MY_SEI_STREAM_BUFF_SIZE	100000
 
 /*----------------------------------------------------------*/
@@ -122,19 +103,7 @@
 /*----------------------------------------------------------*/
 #define MY_FILLER_DATA_BUFF_SIZE 256
 
-/*----------------------------------------------------------*/
-/* Buffer size which is used if the avcbe_encode_picture function returns on each 1-slice */
-/*----------------------------------------------------------*/
-#define MY_DUMMY_NAL_BUFF_SIZE (1024*256)
 
-/*----------------------------------------------------------*/
-/*    Input YUV-data buffer size			                */
-/*----------------------------------------------------------*/
-//#define MY_INPUT_YUV_DATA_BUFF_SIZE (WIDTH_HEIGHT_1_5 *10)
-
-// ORIG #define  OUTPUT_BUF_SIZE (50*1024)
-#define  OUTPUT_BUF_SIZE (256*1024)
-//#define  OUTPUT_BUF_SIZE (256*1024*4)
 
 /*-------------- proto-type declaration ----------------------------*/
 
