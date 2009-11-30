kfish/libshcodecs

commit 816101d715b18744783d85af3686c3b3872f80fe

Modified how stream & work area sizes are calculated. The hardcoded values were excessively large in some cases and too small in others. This change calculated the size of the output stream buffer based on the size of a frame. The work area size is constant for all frame sizes.

src/libshcodecs/shcodecs_encoder.c
@@ -37,29 +37,32 @@ int vpu4_clock_off(void);
 static unsigned long
 work_area_size (SHCodecs_Encoder * encoder)
 {
-	long wh = encoder->width * encoder->height;
-
-	if (wh > 640 * 480) /* > VGA */ {
-		return 1024*512;
-	} else if (wh == 640 * 480) /* VGA */ {
-		return 101376*4;
-	} else /* < VGA */ {
-		return 101376;
-	}
+	/* All sizes fail at 64KB, but work at 65KB */
+	return 65*1024;
 }
 
 static unsigned long
 stream_buff_size (SHCodecs_Encoder * encoder)
 {
 	long wh = encoder->width * encoder->height;
+	long size = wh + wh/2;	/* Size of uncompressed YCbCr420 frame */
 
-	if (wh > 640 * 480) /* > VGA */ {
-		return 1024*256;
-	} else if (wh == 640 * 480) /* VGA */ {
-		return 400000;
-	} else /* < VGA */ {
-		return 160000;
+   	/* Apply minimum compression ratio */
+	if (wh >= 720 * 480) /* >= D1 */ {
+		size = size / 4;
+	} else {
+		size = size / 2;
 	}
+
+	/* Minimum size as this buffer is used for data other than encoded frames */
+	/* TODO min size has not been verified, just takem from sample code */
+	if (size < 160000)
+		size = 160000;
+
+	/* Make size a multiple of 32 */
+	size = (size + 31) & ~31;
+
+	return size;
 }
 
 static void

commit e7c84478aa187a19aad1e39e84ffdc684e68fb87

increase encoder work area size for H.264
This fixes a regression introduced in
816101d715b18744783d85af3686c3b3872f80fe
which caused a deallocation failure with 720p H.264 encoding.

src/libshcodecs/shcodecs_encoder.c
@@ -37,8 +37,12 @@ int vpu4_clock_off(void);
 static unsigned long
 work_area_size (SHCodecs_Encoder * encoder)
 {
-	/* All sizes fail at 64KB, but work at 65KB */
-	return 65*1024;
+	if (encoder->format == SHCodecs_Format_H264) {
+	        return 256*1024;
+	} else {
+	        /* All sizes fail at 64KB, but work at 65KB */
+	        return 65*1024;
+	}
 }
 
 static unsigned long