e2b99645040bb10260ec63a9caa80793e83bda2c
Phil Edworthy <phil.edworthy@renesas.com>
2010-01-21 09:15:24 +0000

    shcodecs-cap: User can now set capture size

--- src/tools/shcodecs-cap.c
+++ src/tools/shcodecs-cap.c
@@ -31,31 +31,33 @@
 #include <assert.h>
 
 #include <getopt.h>		/* getopt_long() */
-
-#include <fcntl.h>		/* low-level i/o */
 #include <unistd.h>
 #include <errno.h>
 
 #include "capture.h"
 #include "framerate.h"
 
-#define CLEAR(x) memset (&(x), 0, sizeof (x))
 
 static void usage(FILE * fp, int argc, char **argv)
 {
 	fprintf(fp,
 		"Usage: %s [options]\n\n"
 		"Options:\n"
-		"-d | --device name   Video device name [/dev/video]\n"
-		"-h | --help  Print this message\n" "", argv[0]);
+		"-d | --device name   Video device name [/dev/video0]\n"
+		"-w | --width         Capture width [320]\n"
+		"-h | --height        Capture height [240]\n"
+		"-s | --size          Capture size [qcif, cif, qvga, vga, d1, 720p]\n"
+		"", argv[0]);
 	fprintf (fp, "\nPlease report bugs to <linux-sh@vger.kernel.org>\n");
 }
 
-static const char short_options[] = "d:hmru";
+static const char short_options[] = "d:w:h:s:";
 
 static const struct option long_options[] = {
 	{"device", required_argument, NULL, 'd'},
-	{"help", no_argument, NULL, 'h'},
+	{"width",  required_argument, NULL, 'w'},
+	{"height", required_argument, NULL, 'h'},
+	{"size",   required_argument, NULL, 's'},
 	{0, 0, 0, 0}
 };
 
@@ -72,6 +74,8 @@ int main(int argc, char **argv)
 {
 	capture *ceu;
 	char *dev_name = "/dev/video0";
+ 	unsigned int width = 320;
+ 	unsigned int height = 240;
 	unsigned int count, x;
 	double time;
 	struct framerate * cap_framerate;
@@ -94,9 +98,37 @@ int main(int argc, char **argv)
 			dev_name = optarg;
 			break;
 
+		case 'w':
+			if (optarg)
+				width = strtoul(optarg, NULL, 10);
+			break;
 		case 'h':
-			usage(stdout, argc, argv);
-			exit(EXIT_SUCCESS);
+			if (optarg)
+				height = strtoul(optarg, NULL, 10);
+			break;
+		case 's':
+			if (optarg) {
+				if (!strncasecmp (optarg, "qcif", 4)) {
+					width = 176;
+					height = 144;
+				} else if (!strncmp (optarg, "cif", 3)) {
+					width = 352;
+					height = 288;
+				} else if (!strncmp (optarg, "qvga", 4)) {
+					width = 320;
+					height = 240;
+				} else if (!strncmp (optarg, "vga", 3)) {
+					width = 640;
+					height = 480;
+				} else if (!strncmp (optarg, "d1", 2)) {
+					width = 720;
+					height = 480;
+				} else if (!strncmp (optarg, "720p", 4)) {
+					width = 1280;
+					height = 720;
+				}
+			}
+			break;
 
 		default:
 			usage(stderr, argc, argv);
@@ -104,7 +136,7 @@ int main(int argc, char **argv)
 		}
 	}
 
-	ceu = capture_open(dev_name, 640, 480);
+	ceu = capture_open(dev_name, width, height);
 	cap_framerate = framerate_new_timer (30.0);
 
 	capture_start_capturing(ceu);
@@ -119,7 +151,8 @@ int main(int argc, char **argv)
 
 	time = (double)framerate_elapsed_time (cap_framerate);
 	time /= 1000000;
-	printf("\nCaptured %d frames (%.2f fps)\n", count, (double)count/time);
+	printf("\nCaptured %d frames at %dx%d (%.2f fps)\n",
+		count, width, height, (double)count/time);
 
 	framerate_destroy (cap_framerate);
 
